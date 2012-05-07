8353a2ffcad5529c65786b5c2c4da64ccfff0fad
torbjorn josefsson <torbjorn.josefsson@gmail.com>
2012-05-07 15:58:09 +0200

    tiny cleanup

--- jquery.holdr.js
+++ jquery.holdr.js
@@ -33,11 +33,12 @@
     var providers = {
       flickholdr: function() {
                     var emptyNodes = processors.defaultProcessor.apply(this);
-                    return $(emptyNodes).each(function(index,item){
-                      var width = item.width();
-                      var height = item.height();
-                      var keyword = item.attr('alt');
-                      var src = 'http://' + settings.provider + '.com/' +
+                    return $(emptyNodes).each(function(index,item) {
+                      var width = item.width(),
+                          height = item.height(),
+                          keyword = item.attr('alt'),
+
+                          src = 'http://' + settings.provider + '.com/' +
                                 (width ? width : settings.defaultWidth) +
                                 '/'+ (height ? width : settings.defaultHeight) +
                                 '/' + (keyword ? keyword: "");
@@ -47,30 +48,34 @@
                   },
       placekitten: function() {
                      var emptyNodes = processors.defaultProcessor.apply(this);
-                     return $(emptyNodes).each(function(index,item){
-                         var width = item.width();
-                         var height = item.height();
-                         var src = 'http://' + settings.provider + '.com/' +
+                     return $(emptyNodes).each(function(index,item) {
+                       var  width = item.width(),
+                            height = item.height(),
+
+                            src = 'http://' + settings.provider + '.com/' +
                                    (width ? width : 200)+'/' +
                                    (height ? width : 300)+'/';
-                         item.attr('src',src);
+
+                            item.attr('src',src);
                      });
                    },
       robohash: function() {
                      var emptyNodes = processors.defaultProcessor.apply(this);
                      return $(emptyNodes).each(function(index,item){
-                         var width = item.width();
-                         var height = item.height();
-                         var keyword = item.attr('alt');
-                         var set = 'set' + Math.floor(Math.random() * (3 - 1 + 1) + 1);
+                        var width = item.width(),
+                            height = item.height(),
+                            keyword = item.attr('alt');
 
-                         var src = 'http://' + settings.provider + '.org/' +
+                        var set = 'set' + Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+                        var src = 'http://' + settings.provider + '.org/' +
                                    (keyword ? keyword: "random") + '.png' +
                                    '?set=' + set +
                                    '&size=' +
                                    (width ? width : 200)+'x' +
                                    (height ? width : 200);
-                         item.attr('src',src);
+
+                          item.attr('src',src);
                      });
                 }
     };
